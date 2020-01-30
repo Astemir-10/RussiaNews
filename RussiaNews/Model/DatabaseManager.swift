b3476629c10677d6c611a086c19d6300fb8ba333
Astemir <shibzuhov2000@mail.ru>
2020-01-30 23:11:58 +0300

    dfdf

---
+++ RussiaNews/Model/DatabaseManager.swift
@@ -0,0 +1,9 @@
+//
+//  DatabaseManager.swift
+//  RussiaNews
+//
+//  Created by Astemir Shibzuhov on 30.01.2020.
+//  Copyright © 2020 Astemir Shibzuhov. All rights reserved.
+//
+
+import Foundation
